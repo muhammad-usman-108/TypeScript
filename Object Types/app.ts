a961d6eae7fe6f4fcd207e14e5a5bd5002c374d8
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-07-16 23:01:57 +0200

    TypeScript: Object Types

--- Object Types/app.ts
+++ Object Types/app.ts
@@ -3,5 +3,14 @@ const person = {
     age: 30
 }
 
+
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Usman',
+//     age: 30
+// }
+
 console.log(person.name);
 console.log(person.age);
