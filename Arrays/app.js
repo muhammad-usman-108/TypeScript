f827c10704df06176b430b037a524efe4eefa432
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-07-18 16:40:43 +0200

    TypeScript: Arrays

---
+++ Arrays/app.js
@@ -0,0 +1,10 @@
+//let favAct : string[];
+var favAct = ['usman', 1, 'test'];
+console.log(favAct);
+for (var _i = 0, favAct_1 = favAct; _i < favAct_1.length; _i++) {
+    var hobby = favAct_1[_i];
+    console.log(hobby);
+    //console.log(hobby.toUpperCase());
+}
+favAct.push('test');
+console.log(favAct);
---
+++ Arrays/app.ts
@@ -0,0 +1,15 @@
+
+//let favAct : string[];
+let favAct = ['usman', 1, 'test'];
+
+
+
+console.log(favAct);
+
+for (const hobby of favAct) {
+    console.log(hobby);
+    //console.log(hobby.toUpperCase());
+}
+
+favAct.push('test');
+console.log(favAct);
