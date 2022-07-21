447fb0dd9589c2767e1f6566b676639b56f5ad55
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-07-22 00:08:35 +0200

    TypeScript: Class

---
+++ Class/app.js
@@ -0,0 +1,11 @@
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.name = n;
+    }
+    Department.prototype.describe = function () {
+        console.log('Department: ' + this.name);
+    };
+    return Department;
+}());
+var accounting = new Department("Accounting");
+accounting.describe();
